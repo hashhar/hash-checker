d26ac22e9afa82dbf144a404f72ba5d4a618a8e4
hashhar <hashhar@outlook.com>
2015-09-05 04:30:24 +0530

    Made a console application to calculate sha1 checksum
    Console application that calculates SHA1 checksum of a file (hard-coded
    into the program) and writes it to a file. All path names are
    hard-coded.

---
+++ hash-check-console/hash-check-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace hash_check_console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var stream = new BufferedStream(File.OpenRead(@"D:\Software\OS\Windows10Pro_x64.iso"), 1200000))
+			using (var cryptoProvider = new SHA1CryptoServiceProvider())
+			{
+				var hash = BitConverter.ToString(cryptoProvider.ComputeHash(stream));
+				using (var file = new StreamWriter(@"D:\output.txt"))
+					file.WriteLine(hash);
+			}
+		}
+	}
+}
+
